0ba85f302b528900548a56b55f0c1c7f4dd9bd58
JesusDSaavedra <iam.jesusd88@gmail.com>
2024-07-05 01:23:50 -0500

    PlayerButton añadido en album

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Pause, Play } from './Player'
 import { usePlayerStore } from '@/store/playerStore'
 
-export  function CardPlayButton({id}) {
+export  function CardPlayButton({id, size = 'small'}) {
 
     const { currentMusic, isPlaying, setCurrentMusic, setIsPlaying } = usePlayerStore(state => state)
 
@@ -26,10 +26,12 @@ export  function CardPlayButton({id}) {
     }
 
 
+    const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
 
   return (
-    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4'>
-        {isPlayingPlaylist ? <Pause/> : <Play />}
+    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400'>
+        {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   )
 }
